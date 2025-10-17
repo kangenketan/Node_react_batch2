29ef05d393275be0e2456d833e08c1649764363d
kangenketan <arif.sambodin@gmail.com>
2025-10-17 16:49:39 +0800

    mantap

--- latihan/hari9/rest-api-prisma-jwt.js/app.js
+++ latihan/hari9/rest-api-prisma-jwt.js/app.js
@@ -1,17 +1,25 @@
-const express = require("express");
 require("dotenv").config();
+
+const express = require("express");
 const cors = require("cors");
+
+const app = express();
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger-output.json");
 const router = require("./src/routes/main");
 const routerCategory = require("./src/routes/categoryRouter");
-const app = express();
 const port = 3000;
 
 var corsOptions = {
   origin: process.env.CORS_ALLOW_LIST.split(","),
   optionsSuccessStatus: 200,
 };
+
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use("/api", router, routerCategory);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 //root
 // app.get("/Movie", getMovies);
 // app.get('Movie/:id', getMoviesByID)
@@ -19,8 +27,6 @@ app.use(express.json());
 //ROUTE MOVIE
 // app.get('/movie',getMovies)
 // app.get('/movie/:id',getMoviesByID)
-
-app.use("/api", router, routerCategory);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
